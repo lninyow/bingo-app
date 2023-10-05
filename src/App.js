e375622f64b2322a1fb2c852cc5ae7d5cb8708ec
Liden <lninyow@gmail.com>
2023-10-05 12:35:33 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [code, setCode] = useState(""); // State to store the input value
+  const [bingoCard, setBingoCard] = useState(null)
+  const [winner, setWinner] = useState(null)
+  const [background, setBackground] = useState('white')
+
+  const handleGetCard = () => {
+    axios.get(`http://www.hyeumine.com/getcard.php?bcode=${code}`)
+    .then(response => {
+        if (!response || !response.data) {
+            throw new Error("Empty or invalid response received");
+        }
+
+        const data = response.data;
+
+        console.log(data);
+        setBingoCard(data);
+
+        if (data === 0) {
+            alert("Bingo Code does not exist!");
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+};
+
+
+
+
+const handleInputChange = (e) => {
+  // Update the 'code' state when input value changes
+  setCode(e.target.value);
+  e.target.value="";
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+                id="code"
+                name="code"
+                type="text"
+                placeholder="Enter your Code"
+                value={code} // Bind input value to the 'code' state
+                onChange={handleInputChange} // Handle input changes
+                />
+      <button onClick={handleGetCard}>Get card</button>
+    </>
   );
 }
 
