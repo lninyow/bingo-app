7d96f51ec11786ae208f4763a2288e20ba5019fe
Liden <lninyow@gmail.com>
2023-10-05 14:18:03 +0800

    ambot ug mana bani

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
         console.log(data);
         setBingoCard(data);
         if (data === 0) {
+          setBingoCard(null);
             alert("Bingo Code does not exist!");
+            setCode("");
         }
     })
     .catch(error => {
@@ -37,7 +39,6 @@ const checkWin = () => {
     const data = response.data;
     setWinner(data)
     console.log(bingoCard.playcard_token)
-
     if(data === 0){
       alert("You have not won yet!")
     }else{
@@ -52,27 +53,40 @@ const checkWin = () => {
 
 
 return (
-  <><div style={{ display: 'flex', alignItems: 'center', width: '100%', height: 'auto', justifyContent: 'center' }}>
+  <><div style={{ display: 'flex', alignItems: 'center', width: '100%', height: '1000', justifyContent: 'center' }}>
     {bingoCard === null ? (
-      <>
+      <div style={{marginTop:50}}>
+        <h1 style={{textAlign:'center'}}>Welcome to Bingo!</h1>
         <input
+        style={{width:150,height:50,borderRadius:10,marginRight:10}}
           id="code"
           name="code"
           type="text"
           placeholder="Enter your Code"
           value={code}
           onChange={(e) => setCode(e.target.value)} />
-        <button onClick={getCard}>Get card</button>
-      </>
+        <button onClick={getCard}
+        style={{padding:20,background:'lightgrey',fontWeight:'bolder',transition:'background 0.3s',cursor:'pointer',borderRadius:10}}
+        onMouseOver={(e) => {
+          e.target.style.background = 'lightblue';
+          e.target.style.color = 'white';
+        }}
+        onMouseOut={(e) => {
+          e.target.style.background = 'lightgrey';
+          e.target.style.color = 'black';
+        }} 
+        >
+          GET CARD</button>
+      </div>
     ) : (
       <>
         {Object.keys(bingoCard.card).map((key, i) => {
           return (
-            <div key={i} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '1px' }}>
+            <div key={i} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '1px',gap:3 }}>
               <h1>{key}</h1>
               {bingoCard.card[key].map((col, j) => {
                 return (
-                  <div key={j} style={{ display: 'flex', flexDirection: 'column', border: '1px solid black', width: '100px', height: '100px', alignItems: 'center', justifyContent: 'center', background: ' white' }}>
+                  <div key={j} style={{ color:'white',fontSize:32,display: 'flex', flexDirection: 'column', border: '2px solid black', width: '100px', height: '100px', alignItems: 'center', justifyContent: 'center', background: 'purple'}}>
                     <p>{col}</p>
                   </div>
                 );
@@ -85,26 +99,27 @@ return (
       </>
     )}
   </div>
-  {bingoCard === null ? <> </> : <> 
+  {bingoCard === null ? <> </> :  
   <div style={{width:'100%',textAlign:'center'}}>
   <button 
   onClick={checkWin}
   style={{
+    width:300,
     padding: 20,
     marginTop: 10,
     background: 'white',
     fontWeight: 'bold',
-    transition: 'background 0.3s', // Add transition for smooth effect
-    cursor: 'pointer', // Change cursor on hover
+    transition: 'background 0.3s',
+    cursor: 'pointer', 
   }}
-  onMouseOver={(e) => e.target.style.background = 'lightblue'} // Change color on hover
-  onMouseOut={(e) => e.target.style.background = 'white'} // Restore color on mouse out
+  onMouseOver={(e) => e.target.style.background = 'lightblue'} 
+  onMouseOut={(e) => e.target.style.background = 'white'} 
 >
   CHECK WIN
 </button>
 
     </div>
-    </>}
+    }
     </>
 );
 
